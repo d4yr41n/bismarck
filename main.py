3b01c18c950de24714186b6cc7e8da4c082415ec
Day Rain <121486178+d4yr41n@users.noreply.github.com>
2024-11-22 21:13:14 +0600

    update: main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Game:
         j for i in range(1, 8) for j in range(i * 9 + 1, i * 9 + 8)
     )
     format = "··HSM"
-    format = "·#HSM"
+    # format = "·#HSM"
 
     def __init__(self):
         self.setup()
